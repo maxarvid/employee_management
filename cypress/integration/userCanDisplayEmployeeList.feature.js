e1d678146161c37c4dcf5cfaad5dea2bfc6c41f3
maxarvid <max.arvid.anderson@gmail.com>
2021-09-13 15:47:49 +0200

    adds first cypress test for header presence

---
+++ cypress/integration/userCanDisplayEmployeeList.feature.js
@@ -0,0 +1,10 @@
+describe('Display list of employees', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  });
+
+  it('displays a header', () => {
+    cy.get('#header').should('contain', 'Employee List')
+  });
+  
+})
