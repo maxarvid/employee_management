82484ec2bcb4c6ec2c05e34de8363bbac07e9098
maxarvid <max.arvid.anderson@gmail.com>
2021-09-13 16:48:30 +0200

    adds partial implementation to EmployeeList.jsx: axios call to the API, mapping the data using hooks

--- src/components/EmployeeList.jsx
+++ src/components/EmployeeList.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const EmployeeList = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [employees, setEmployees] = useState([]);
 
-export default EmployeeList
+  const fetchData = async () => {
+    await axios.get("https://reqres.in/api/users");
+    setEmployees(response.data.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const employeeList = employees.map((employee) => {
+    return (
+      <li key={employee.id} className="employee-item">
+        <p>
+          {employee.first_name} {employee.last_name}
+        </p>
+      </li>
+    );
+  });
+
+  return <ul id="employee-list">{employeeList}</ul>;
+};
+
+export default EmployeeList;
