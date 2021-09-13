5872530388d37c742f9952bb141cb0066bb826a1
maxarvid <max.arvid.anderson@gmail.com>
2021-09-13 18:28:36 +0200

    adds EmployeeModal.jsc code as functional component with props

--- src/components/EmployeeModal.jsx
+++ src/components/EmployeeModal.jsx
@@ -1,11 +1,35 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
+import { Modal, Image, Header, Button } from "semantic-ui-react";
 
-const EmployeeModal = () => {
+const EmployeeModal = ({ employee }) => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Modal
+      onClose={() => setOpen(false)}
+      onOpen={() => setOpen(true)}
+      open={open}
+      trigger={
+        <Button size="tiny" positive className="view-button">
+          View
+        </Button>
+      }
+    >
+      <Modal.Content image id="modal-container">
+        <Image className="image" size="small" src={employee.avatar} wrapped />
+        <Modal.Description>
+          <Header className="name">
+            {employee.first_name} {employee.last_name}
+          </Header>
+          <p className="email">Email: {employee.email}</p>
+        </Modal.Description>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button color="black">Edit</Button>
+        <Button negative>Delete</Button>
+      </Modal.Actions>
+    </Modal>
+  );
+};
 
-export default EmployeeModal
+export default EmployeeModal;
