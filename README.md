40b908b6ab0659ab76c7e3c4e56d8c7042d23469
maxarvid <max.arvid.anderson@gmail.com>
2021-09-13 15:53:19 +0200

    adds test to check for 6 items in employee-list

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ Week 3 CA Fullstack Webdev project 1: Build an employee management app in React.
 As an employee manager
 In order to be able to work with employee management
 I would like to access a list of employees
+
+As an employee manager
+In order to be more efficient
+I would like to access six employees profiles at once
 ```
 
 
--- cypress/integration/userCanDisplayEmployeeList.feature.js
+++ cypress/integration/userCanDisplayEmployeeList.feature.js
@@ -6,5 +6,12 @@ describe('Display list of employees', () => {
   it('displays a header', () => {
     cy.get('#header').should('contain', 'Employee List')
   });
+
+  it('displays a list with 6 items', () => {
+    cy.get('#employee-list').within(()=>{
+      cy.find('.employee-item').should('have.length', 6)
+    })
+    
+  });
   
 })
